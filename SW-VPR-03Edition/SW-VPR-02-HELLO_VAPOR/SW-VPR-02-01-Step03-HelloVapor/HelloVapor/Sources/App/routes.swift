13b9e8ff7e56b794c075667e72737f9ae7c19f00
Ignasi  Perez <perezignasi@gmail.com>
2021-06-17 14:26:55 +0200

    SW-VPR-02-01-Step03-HelloVapor

---
+++ SW-VPR-03Edition/SW-VPR-02-HELLO_VAPOR/SW-VPR-02-01-Step03-HelloVapor/HelloVapor/Sources/App/routes.swift
@@ -0,0 +1,48 @@
+import Vapor
+
+
+func routes(_ app: Application) throws {
+  
+  app.get { req in
+    return "It works!"
+  }
+  
+  
+  app.get("hello") { req -> String in
+    return "Hello, world!"
+  }
+  
+  
+  app.get("hello", "vapor") { req -> String in
+    return "Hello Vapor!"
+  }
+  
+  // 1
+  app.get("hello", ":name") { req -> String in
+    // 2
+    guard let name = req.parameters.get("name")
+    else {
+      throw Abort(.internalServerError)
+    }
+    
+    //3
+    return "Hello, \(name)!"
+  }
+  
+  
+  app.post("info") { req -> InfoResponse in
+    let data = try req.content.decode(InfoData.self)
+    
+    return InfoResponse(request: data)
+  }
+}
+
+
+struct InfoData: Content {
+  let name: String
+}
+
+
+struct InfoResponse: Content {
+  let request: InfoData
+}
